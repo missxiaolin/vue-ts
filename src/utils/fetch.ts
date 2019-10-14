a6f33b7192306301d80a5f26ec2a4f101385115d
xiaolin <462441355@qq.com>
2019-10-14 14:13:18 +0800

    增加可连续调用

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,6 +1,16 @@
 import axios, { AxiosResponse, AxiosRequestConfig } from 'axios'
+import config from "@/config"
+import router from "@/router/index"
+
+const isToIndex: string[] = [
+    "/index"
+]
 
 const baseURL = process.env.VUE_APP_WEB_API
+
+import {
+    Dialog,
+} from 'vant';
 
 class HttpRequest {
     public queue: any // 请求的url集合
@@ -9,6 +19,9 @@ class HttpRequest {
         this.queue = {}
     }
 
+    /**
+     * @param url 
+     */
     destroy(url: string) {
         delete this.queue[url]
         if (!Object.keys(this.queue).length) {
@@ -16,45 +29,110 @@ class HttpRequest {
         }
     }
 
-    // 发起请求
+    /**
+     * 发起请求
+     * @param options 
+     */
     async request(options: AxiosRequestConfig) {
         options.baseURL = baseURL
+        return new Promise((resolve, reject) => {
+            this._request(options, resolve, reject)
+        })
+    }
+
+    /**
+     * 异步使用
+     * @param options 
+     * @param resolve 
+     * @param reject 
+     */
+    _request(options: AxiosRequestConfig, resolve: any, reject: any) {
         const instance = axios.create()
-        await this.interceptors(instance, options.url)
+        this.interceptors(instance, options, resolve, reject)
         return instance(options)
     }
 
-    // 拦截器
-    interceptors(instance: any, url?: string) {
+
+
+    /**
+     * 拦截器
+     * @param instance 
+     * @param options 
+     * @param resolve 
+     * @param reject 
+     */
+    async interceptors(instance: any, options: AxiosRequestConfig, resolve: any, reject: any) {
         // 请求拦截
         instance.interceptors.request.use((config: AxiosRequestConfig) => {
             // 添加全局的loading...
             if (!Object.keys(this.queue).length) {
             // show loading
             }
-            if (url) {
-                // 做相同请求同时发起拦截
-                // this.queue[url] = true
-            }
+            config.headers.token = localStorage.getItem("token")
             return config
         }, (error: any) => {
             console.error(error)
-            Promise.reject(error)
+            reject(error)
         })
         // 响应拦截
         instance.interceptors.response.use((res: AxiosResponse) => {
-            if (url) {
-                // this.destroy(url)
-            }
             const { data } = res
-            return Promise.resolve(data)
-        }, (error: any) => {
-            if (url) {
-                // this.destroy(url)
+            // 如果不是登录先登录再次访问api
+            if (data.sussess !== config.ERR_OK && (data.errorCode === config.TOKEN_ERROR || data.errorCode === config.TOKEN_OVERDUE)) { // toke失效
+                this.userLogin().then((res: any) => {
+                    this._request(options, resolve, reject)
+                }, (rej: any) => {
+                    console.error(rej)
+                })
+            } else if (data.sussess !== config.ERR_OK && data.errorCode === config.NOTICE_STRONG) { // 强公告
+                Dialog.alert({
+                    message: data.model.content,
+                    confirmButtonText: data.model.button || "确认"
+                }).then(() => {
+                    // 如果不是首页跳转到首页
+                    if (isToIndex.indexOf(router.currentRoute.path) !== 0) {
+                        router.replace({
+                            path: '/index'
+                        })
+                    }
+                })
+                reject(data)
+            } else {
+                resolve(data)
             }
+            
+        }, (error: any) => {
             console.error(error)
-            return Promise.reject(error)
+            reject(error)
         })
+    }
+
+    /**
+     * 用户登录
+     * @param data 
+     */
+    async userLogin() {
+        let userEncryption: any = localStorage.getItem('userEncryption')
+        // 判断是否有加密字符串
+        if (!userEncryption) {
+            // 如果没有重新调用第三方获取加密字符串
+            return
+        }
+        let options: any = {
+            url: "/h5/auth/login",
+            method: "POST",
+            data: {
+                externalUserId: userEncryption
+            }
+        }
+        let res: any = await this.request(options)
+        if (res.success === config.ERR_OK ) {
+            if (res.model && res.model.token) {
+                localStorage.setItem('token', res.model.token)
+                return true
+            }
+        }
+        throw new Error('token fetch failed')
     }
 }
 
