d85f05788adfee9de7b52c118c33da8d5861d04f
xiaolin <462441355@qq.com>
2019-10-23 21:43:11 +0800

    fetch

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -77,62 +77,12 @@ class HttpRequest {
         // 响应拦截
         instance.interceptors.response.use((res: AxiosResponse) => {
             const { data } = res
-            // 如果不是登录先登录再次访问api
-            if (data.sussess !== config.ERR_OK && (data.errorCode === config.TOKEN_ERROR || data.errorCode === config.TOKEN_OVERDUE)) { // toke失效
-                this.userLogin().then((res: any) => {
-                    this._request(options, resolve, reject)
-                }, (rej: any) => {
-                    console.error(rej)
-                })
-            } else if (data.sussess !== config.ERR_OK && data.errorCode === config.NOTICE_STRONG) { // 强公告
-                Dialog.alert({
-                    message: data.model.content,
-                    confirmButtonText: data.model.button || "确认"
-                }).then(() => {
-                    // 如果不是首页跳转到首页
-                    if (isToIndex.indexOf(router.currentRoute.path) !== 0) {
-                        router.replace({
-                            path: '/index'
-                        })
-                    }
-                })
-                reject(data)
-            } else {
-                resolve(data)
-            }
+            resolve(data)
             
         }, (error: any) => {
             console.error(error)
             reject(error)
         })
-    }
-
-    /**
-     * 用户登录
-     * @param data 
-     */
-    async userLogin() {
-        let userEncryption: any = localStorage.getItem('userEncryption')
-        // 判断是否有加密字符串
-        if (!userEncryption) {
-            // 如果没有重新调用第三方获取加密字符串
-            return
-        }
-        let options: any = {
-            url: "/h5/auth/login",
-            method: "POST",
-            data: {
-                externalUserId: userEncryption
-            }
-        }
-        let res: any = await this.request(options)
-        if (res.success === config.ERR_OK ) {
-            if (res.model && res.model.token) {
-                localStorage.setItem('token', res.model.token)
-                return true
-            }
-        }
-        throw new Error('token fetch failed')
     }
 }
 
