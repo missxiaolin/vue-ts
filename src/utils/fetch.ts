20684180bf1047790c1dc5d350141ae9389d7204
xiaolin <462441355@qq.com>
2019-11-05 11:24:23 +0800

    * 解决报错

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse, AxiosRequestConfig } from 'axios'
-import config from "@/config"
+// import config from "@/config"
 import router from "@/router/index"
 
 const isToIndex: string[] = [
