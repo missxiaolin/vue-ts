4059fa7d14d54469931d32fff84bf01fab5d1e9e
xiaolin <462441355@qq.com>
2019-11-08 10:02:03 +0800

    * 动态路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import routes from './router'
+let routes: any = []
+
+// 利用require.context()自动引入article.ts和user.ts
+const routerContext = require.context('./', true, /\.ts$/)
+routerContext.keys().forEach(route => {
+  // 如果是根目录的 index.js 、不处理
+  if (route.startsWith('./index')) {
+    return
+  }
+  const routerModule = routerContext(route)
+  console.log(routerModule)
+  /**
+   * 兼容 import export 和 require module.export 两种规范
+   */
+  routes = routes.concat(routerModule.default || routerModule)
+})
 
 Vue.use(Router)
 
